68183e3d8519ee7253dc79775845cf8312555b79
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2024-09-10 13:12:39 +0300

    Created and added search panel

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
   <top-nav-menu></top-nav-menu>
+  <top-search></top-search>
+  <home-page></home-page>
 </template>
 
 <script>
 
 
 import TopNavMenu from "@/components/top-nav-menu";
+import HomePage from "@/pages/home-page";
+import TopSearch from "@/components/top-search";
 export default {
   name: 'App',
-  components: {TopNavMenu},
+  components: {TopSearch, HomePage, TopNavMenu},
 }
 </script>
 
---
+++ src/components/top-search.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="container mt-3">
+    <div class="row">
+      <div class="col-lg-12">
+        <div class="input-group">
+          <input class="form-control form-control-lg" type="text" placeholder="Пошук серед більше ніж 300 000 товарів" aria-label="Search example">
+          <button class="btn btn-primary" type="button"><i class="bi bi-search"></i></button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "top-search"
+}
+</script>
+
+<style scoped>
+
+</style>
