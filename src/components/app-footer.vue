42cae9e8d69fc1fe6f2c9c9f1ac114082bdc391b
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2024-09-16 12:08:35 +0300

    finished footer block

--- src/components/app-footer.vue
+++ src/components/app-footer.vue
@@ -2,7 +2,7 @@
     <footer class="footer">
         <div class="container py-5">
           <div class="row">
-            <div class="col-lg-9">
+            <div class="col-xl-8 col-lg-12">
               <div class="footer-menu">
                 <div class="footer-menu__container footer-menu__container-mta">
                   <div class="footer-menu__container-title">MTA</div>
@@ -138,8 +138,26 @@
                 </div>
               </div>
             </div>
-            <div class="col-lg-3">2</div>
-          </div>
+            <div class="col-xl-4 col-lg-12">
+              <div class="card subscribe__block ms-lg-5" style="width: 15rem">
+                <div class="card-body">
+                  <img
+                      src="../assets/images/footer_subscribe.webp"
+                      class="card-img-top"
+                      alt="Card image"
+                  >
+                  <div class="d-flex flex-column">
+                    <div><span class="footer-subscribe__left_title">Всі акційні пропозиції</span></div>
+                    <div class="input-group mt-4">
+                      <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+                      <button class="btn btn-primary" type="button"><i class="bi bi-send"></i></button>
+                    </div>
+                    <div class="subscribe__form_notation">Я погоджуюссь із політикою обробки</div>
+                  </div>
+                </div>
+              </div>
+            </div>
+           </div>
         </div>
     </footer>
 </template>
@@ -153,7 +171,11 @@ export default {
 <style scoped>
   .footer {
     background-color: #F8FAFB;
+    max-width: 100%;
+    overflow-x: hidden;
   }
+
+
 
   .footer-menu {
     display: grid;
@@ -207,5 +229,140 @@ export default {
 
   .footer-menu__contacts_item {
     margin-bottom: 24px;
+  }
+
+
+  /*.footer-subscribe {*/
+  /*  background: #f6fafc;*/
+  /*  margin-bottom: 45px;*/
+  /*  display: flex;*/
+  /*  justify-content: center;*/
+  /*}*/
+
+  /*.footer-subscribe__container {*/
+  /*  width: 100%;*/
+  /*  position: relative;*/
+  /*}*/
+
+  .content-container {
+    max-width: 1420px;
+    margin: 0 auto;
+  }
+
+  /*.footer-subscribe__wrapper {*/
+  /*  position: absolute;*/
+  /*  right: 0;*/
+  /*  top: -70px;*/
+  /*  display: flex;*/
+  /*  flex-direction: column;*/
+  /*  align-items: center;*/
+  /*  width: 430px;*/
+  /*  padding: 10px 38px 50px;*/
+  /*  border-radius: 50px 50px 0 0;*/
+  /*  background: #d5e6fa;*/
+  /*}*/
+
+  .footer-subscribe__left {
+    order: 1;
+  }
+
+  .footer-subscribe__left_title {
+    margin-bottom: 16px;
+    color: #ed9200;
+    font-size: 1.75rem;
+    line-height: 2.0625rem;
+    font-weight: 500;
+    text-transform: uppercase;
+  }
+
+  .footer-subscribe__left_text {
+    margin-bottom: 48px;
+    color: #333;
+    font-size: .875rem;
+    line-height: 1.0625rem;
+    text-align: center;
+  }
+
+  .footer-subscribe__bg {
+    order: 0;
+    width: 320px;
+    height: 214px;
+    margin-bottom: 27px;
+    background-size: auto 100%;
+    background-position: 30px;
+    background-image: url(../assets/images/footer_subscribe.webp)
+  }
+
+  .subscribe__form {
+    position: relative;
+  }
+
+  .subscribe__form input {
+    border-radius: 10px;
+  }
+  .footer-subscribe__wrapper {
+    position: relative;
+    margin-bottom: 2px;
+  }
+
+  .footer-subscribe__form {
+    order: 2;
+    width: 100%;
+  }
+  .subscribe__form_notation {
+    text-align: center;
+    color: #999;
+    font-size: .625rem;
+    line-height: 1.375rem;
+  }
+
+  @media (max-width: 1600px) {
+    .footer-menu {
+      justify-content: space-between;
+      grid-template-columns: 1rem auto;
+    }
+    /*.footer-subscribe__container {*/
+    /*  position: relative;*/
+    /*  padding: 0 21px;*/
+    /*}*/
+    /*.footer-subscribe__wrapper {*/
+    /*  position: static;*/
+    /*  width: 100%;*/
+    /*  height: auto;*/
+    /*  flex-direction: row;*/
+    /*  justify-content: space-between;*/
+    /*  padding: 36px 150px 20px 162px;*/
+    /*}*/
+  }
+  .subscribe {
+    max-width: 100%;
+  }
+  @media (max-width: 991px) {
+    .footer-menu {
+      grid-template-columns: 135px 160px 180px;
+      -moz-column-gap: 62px;
+      column-gap: 62px;
+    }
+  }
+  @media (max-width: 767px) {
+    .footer-menu {
+      padding: 0 16px;
+      -moz-column-gap: 22px;
+      column-gap: 22px;
+      row-gap: 58px;
+      justify-content: center;
+      grid-template-columns: 142px 165px;
+    }
+  }
+
+  @media (max-width: 479px) {
+    .footer-menu {
+      padding: 0 15px;
+      grid-template-columns: 142px 166px;
+      -moz-column-gap: 22px;
+      column-gap: 22px;
+      row-gap: 60px;
+      justify-content: space-between;
+    }
   }
 </style>
