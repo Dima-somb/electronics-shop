893f544f6a15bed0af3eb0f7394efcddb2415307
dmytro-kondratenko <dmyto.kondratenko@sombrainc.com>
2024-09-29 17:25:00 +0300

    Implemented search panel

--- src/components/top-search.vue
+++ src/components/top-search.vue
@@ -3,8 +3,18 @@
     <div class="row">
       <div class="col-lg-12">
         <div class="input-group">
-          <input class="form-control form-control-lg" type="text" placeholder="Пошук серед більше ніж 300 000 товарів" aria-label="Search example">
+          <input class="form-control form-control-lg"
+                 type="text" placeholder="Пошук серед більше ніж 300 000 товарів"
+                 aria-label="Search example"
+                 v-model="searchTerm"
+                 @input="filterProducts"
+          >
           <button class="btn btn-primary" type="button"><i class="bi bi-search"></i></button>
+        </div>
+        <div class="list-group search-container p-2" v-if="filteredProducts.length > 0">
+          <a href="#" class="list-group-item list-group-item-action search__link" v-for="product in filteredProducts" :key="product.id" aria-current="true">
+            {{ product }}
+          </a>
         </div>
       </div>
     </div>
@@ -12,10 +22,56 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "top-search"
+  name: "top-search",
+
+  data() {
+    return {
+      products: [],
+      filteredProducts: [],
+      searchTerm: ''
+    }
+  },
+
+  methods: {
+    async fetchProducts() {
+      try {
+        const fetchedProducts = await axios.get(`/categories`);
+
+        if(fetchedProducts.data.length > 0) {
+          this.products = fetchedProducts.data.flatMap(category => category.products.map(product => product.name));
+        }
+
+      } catch (e) {
+        console.log(e)
+      }
+    },
+
+    filterProducts() {
+      if(this.searchTerm) {
+        this.filteredProducts = this.products.filter(product =>
+            product.toLowerCase().includes(this.searchTerm.toLowerCase())
+        )
+      } else {
+        this.filteredProducts = ''
+      }
+    }
+  },
+
+  created() {
+    this.fetchProducts();
+  }
 }
 </script>
 
 <style scoped>
+.search__link {
+  border: 0;
+}
+
+.search-container {
+  box-shadow: rgba(100, 100, 111, 0.2) 0px 7px 29px 0px;
+}
 </style>
